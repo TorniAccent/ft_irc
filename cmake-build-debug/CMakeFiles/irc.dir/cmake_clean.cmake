625bb8df8adae61efac081e0fc426681c2a90e89
mchantel <mikaerm65@gmail.com>
2021-10-03 19:50:26 +0300

    cmake init

---
+++ cmake-build-debug/CMakeFiles/irc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/irc.dir/main.cpp.o"
+  "irc"
+  "irc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/irc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
